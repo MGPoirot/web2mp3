c1e09669d9a43fe857f67380046f0f13007fd5c9
MGPoirot <38399483+MGPoirot@users.noreply.github.com>
2023-04-16 02:31:07 +0200

    split utils into setup and utils

---
+++ web2mp3/setup.py
@@ -0,0 +1,115 @@
+import dotenv
+from spotipy.oauth2 import SpotifyClientCredentials
+from importlib.machinery import SourceFileLoader
+import os
+import spotipy
+import eyed3
+eyed3.log.setLevel("ERROR")
+
+
+def get_settings_path() -> str:
+    settings_file = 'settings.txt'
+    try:
+        full_settings_file = os.path.join(os.path.dirname(__file__), settings_file)
+    except NameError:
+        full_settings_file = os.path.join(os.getcwd(), settings_file)
+    return full_settings_file
+
+
+def import_settings():
+    settings_path = get_settings_path()
+    settings_module = SourceFileLoader('settings', settings_path)
+    return settings_module.load_module()
+
+
+def run_setup_wizard():
+    """
+    Runs the setup wizard for Web2MP3 and stores user input in a ".env" file.
+
+    Prompts user for input regarding:
+     - the Web2MP3 home directory
+     - music download directory
+     - Spotify username (optional)
+     - Spotify client ID
+     - Spotify client
+    Validates user input and writes it to a ".env" file.
+
+    :return: None
+    """
+    web2mp3home = os.getcwd()
+    music_dir_default = os.path.join(web2mp3home, "Music")
+
+    sfy_validator = lambda ans: all(c.isdigit() or c.islower() for c in ans) and len(ans) == 32
+    pth_validator = lambda pth: os.path.isdir(os.path.dirname(pth.encode('unicode_escape')))
+
+    qs = {
+        'HOME_DIR':                ('Web2MP3 home directory',
+                                    pth_validator,  web2mp3home),
+        'MUSIC_DIR':               ('Music download directory',
+                                    pth_validator,  music_dir_default),
+        '# Spotify username':      ('Spotify username (optional)',
+                                    lambda x: True, 'None'),
+        'SPOTIPY_CLIENT_ID':       ('Spotify client ID',
+                                    sfy_validator,  None),
+        'SPOTIPY_CLIENT_SECRET':   ('Spotify client secret',
+                                    sfy_validator,  None),
+    }
+    print("                         , - ~ ~ ~ - ,                           \n"
+          "                     , '   WEB 2 MP3   ' ,                       \n"
+          "                   ,                       ,                     \n"
+          "                  ,         |~~~~~~~|       ,                    \n"
+          "                 ,          |~~~~~~~|        ,                   \n"
+          "                 ,          |       |        ,                   \n"
+          "                 ,      /~~\|   /~~\|        ,                   \n"
+          "                  ,     \__/    \__/        ,                    \n"
+          "                   ,                       ,                     \n"
+          "                     ,Music Download CLI, '                      \n"
+          "                       ' - , _ _ _ ,  '                          \n")
+    print('Welcome to Web2MP3. No setup file (".env") was found. The setup  \n'
+          'wizard will as a few questions to set up Web2MP3. Answers are    \n'
+          'secret and stored in the ".env" file. If available, a proposed   \n'
+          'default is suggested in brackets:                                \n')
+    for i, (k, (question, validator, default)) in enumerate(qs.items(), 1):
+        while True:
+            q_fmt = f'What is your {question}?'
+            default = default if not os.environ.get(k) else os.environ.get(k)
+            d_fmt = f'[{repr(default)}]' if default else ''
+            answer = input(f'  {i}. {q_fmt.ljust(settings.print_space)}\n'
+                           f'    {d_fmt}\n')
+            answer = default if not answer and default else answer
+            if validator(answer):
+                with open('.env', 'a') as f:
+                    f.write(f'{k}={repr(answer)}\n')
+                break
+            else:
+                print(f'     "{answer}" is not a valid {question}')
+    print('Secrets successfully stored.\n'
+          'Web2MP3 set up successful.')
+
+# Import public settings
+settings = import_settings()
+
+# Check if Web2MP3 has been set up.
+if not dotenv.find_dotenv():
+    print("No environment file found. Initiating setup wizard.")
+    run_setup_wizard()
+dotenv.load_dotenv()
+
+# Check if setup file is complete, if not, resume setup
+env_keys = 'HOME_DIR', 'MUSIC_DIR', 'SPOTIPY_CLIENT_ID', 'SPOTIPY_CLIENT_SECRET'
+env_vals = [os.environ.get(v) for v in env_keys]
+if None in env_vals:
+    print("Incomplete environment file found. Resuming setup.")
+    run_setup_wizard()
+dotenv.load_dotenv()
+
+# Define other paths
+home_dir = os.environ.get("HOME_DIR")
+music_dir = os.environ.get("MUSIC_DIR")
+daemon_dir = os.path.join(home_dir, '.daemons', 'daemon-{}.tmp')
+log_dir = os.path.join(home_dir, '.logs', '{}.json')
+song_db_file = os.path.join(home_dir, 'song_db.pkl')
+
+# Access Spotify API
+spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+
--- web2mp3/tag_manager.py
+++ web2mp3/tag_manager.py
@@ -6,11 +6,11 @@ import eyed3
 import requests
 import shutil
 from time import sleep
-from utils import Logger
 
 
 def get_track_tags(track_item: dict, logger=print, do_light=False) -> pd.Series:
-    # in do_light mode we only get title, album and artist information; just enough to do matching.
+    # in do_light mode we only get title, album and artist information;
+    # just enough to do matching.
     read_timeout = False
 
     tag_dict = {
@@ -30,19 +30,23 @@ def get_track_tags(track_item: dict, logger=print, do_light=False) -> pd.Series:
             except TimeoutError:
                 if not read_timeout:
                     read_timeout = True
-                    logger('get_track_tags encountered a SpotiPy API ReadTimeout error')
+                    logger('get_track_tags encountered'
+                           ' a SpotiPy API ReadTimeout error')
                     sleep(2)
                 else:
-                    logger('get_track_tags failed after a SpotiPy API ReadTimeout error')
+                    logger('get_track_tags failed after'
+                           ' a SpotiPy API ReadTimeout error')
                     return
         if features is not None:
+            artist_items = track_item['artists']
+            genres = [spotify.artist(a['uri'])['genres'] for a in artist_items]
             tag_dict.update({
                 'bpm': int(features['tempo']),
-                'artist': '; '.join([a['name'] for a in track_item['artists']]),
+                'artist': '; '.join([a['name'] for a in artist_items]),
                 'internet_radio_url': track_item['uri'],
                 'cover': track_item['album']['images'][0]['url'],
                 'disc_num': spotify.track(track_item['uri'])['disc_number'],
-                'genre': '; '.join(flatten([spotify.artist(a['uri'])['genres'] for a in track_item['artists']])),
+                'genre': '; '.join(flatten(genres)),
                 'release_date': track_item['album']['release_date'],
                 'recording_date': track_item['album']['release_date'],
                 'tagging_date': datetime.now().strftime('%Y-%m-%d'),
@@ -54,32 +58,39 @@ def get_track_tags(track_item: dict, logger=print, do_light=False) -> pd.Series:
 
 def manual_track_tags(market='NL') -> pd.Series:
     tag_dict = {
-        'album': input('>> Album name?'.ljust(print_space)) or None,
-        'album_artist': input('>> Artist name?'.ljust(print_space)),
+        'album': input('>>> Album name?'.ljust(print_space)) or None,
+        'album_artist': input('>>> Artist name?'.ljust(print_space)),
         'artist': None,
         'internet_radio_url': 'manual',
-        'cover': input('>> Cover URL?'.ljust(print_space)) or None,
+        'cover': input('>>> Cover URL?'.ljust(print_space)) or None,
         'disc_num': 1,
         'genre': None,
-        'release_date': input('>> Album year?'.ljust(print_space)) or None,
+        'release_date': input('>>> Album year?'.ljust(print_space)) or None,
         'tagging_date': datetime.now().strftime('%Y-%m-%d'),
-        'title': input('>> Track name?'.ljust(print_space)),
-        'track_num': input('>> Track No.?'.ljust(print_space)) or None,
+        'title': input('>>> Track name?'.ljust(print_space)),
+        'track_num': input('>>> Track No.?'.ljust(print_space)) or None,
     }
     tag_series = pd.Series(tag_dict)
     tag_series.artist = tag_series.album_artist
     tag_series.recording_date = tag_series.release_date
     if tag_series.album is None:
         tag_series.album = tag_series.title
-    found_artist = spotify.search(q=tag_series.artist, market=market, limit=1, type='artist')['artists']['items'][0]
-    is_artist = input(f'Is this the artist you were looking for? "{found_artist["name"]}" [Yes]/No') or 'Yes'
+    found_artist = spotify.search(
+        q=tag_series.artist,
+        market=market,
+        limit=1,
+        type='artist'
+    )['artists']['items'][0]
+    is_artist = input(f'>>> Is this the artist you were looking for? '
+                      f'"{found_artist["name"]}" [Yes]/No') or 'Yes'
     if not input_is('No', is_artist):
         tag_series.artist = found_artist["name"]
         tag_series.genre = ';'.join(found_artist['genres'])
     return tag_series
 
 
-def set_file_tags(mp3_tags: dict, file_name: str, audio_source_url=None, logger=print):
+def set_file_tags(mp3_tags: dict, file_name: str, audio_source_url=None,
+                  logger=print):
     # Set mp3 file meta data
     audiofile = eyed3.load(file_name)
     for args in mp3_tags.items():
@@ -101,6 +112,6 @@ def download_cover_img(cover_img_path: str, cover_img_url: str, logger=print):
                 shutil.copyfileobj(res.raw, f)
             logger('Image Downloaded'.ljust(print_space), cover_img_path)
         except FileNotFoundError:
-            raise FileNotFoundError('This folder was not suitable')
+            raise FileNotFoundError('This folder was not suitable.')
     else:
-        raise ConnectionError('Image Couldn\'t be retrieved')
+        raise ConnectionError('Album cover image could not be retrieved.')
